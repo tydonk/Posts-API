7b39e74605d9dc446ba9bd8db0f85fede8c89e83
Tydon Kommel <tydonk@Tydons-MacBook-Pro.local>
2017-02-11 20:12:20 -0800

    added 'post_get' endpoint

--- posts/api.py
+++ posts/api.py
@@ -8,3 +8,13 @@ from . import decorators
 from posts import app
 from .database import session
 
+@app.route("/api/posts", methods=["GET"])
+def posts_get():
+    """ Get a list of posts """
+
+    # Get the posts from the database
+    posts = session.query(models.Post).order_by(models.Post.id)
+
+    # Convert the posts to JSON and return a response
+    data = json.dumps([post.as_dictionary() for post in posts])
+    return Response(data, 200, mimetype="application/json")
